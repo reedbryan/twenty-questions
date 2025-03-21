55fc07ccb0d4ced68677b1d21a7df56a0e556c14
Reed Bryan <reed.f.bryan@gmail.com>
2025-03-21 14:44:21 -0700

    fix filpath

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
-import { formatPrompt } from 'prompt.js';
+import { formatPrompt } from './prompt.js';
 
 function App() {
     const [question, setQuestion] = useState('');
